580d73e2d3297b88cf1a0a3814408132d95e2a88
annacho10 <74904726+annacho10@users.noreply.github.com>
2021-01-05 14:41:17 +0000

    Moved file

---
+++ src/CurrentWeather.js
@@ -0,0 +1,25 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import CurrentTemperature from "./CurrentTemperature";
+
+export default function CurrentWeather(props) {
+  return (
+    <div>
+      <h2>{props.data.city}</h2>
+      <WeatherIcon code={props.data.icon} size={64} />
+      <CurrentTemperature celcius={props.data.temperature} />
+      <h3 className="text-capitalize">{props.data.description}</h3>
+      <h4>
+        H:{Math.round(props.data.maxTemperaure)}° L:
+        {Math.round(props.data.minTemperature)}°
+      </h4>
+      Last updated: <FormattedDate date={props.data.date} />
+      <div>
+        <small>Wind</small> {Math.round(props.data.wind)}mph{" "}
+        <small>Visibility</small> {props.data.visibility}km{" "}
+        <small>Humidity</small> {props.data.humidity}%
+      </div>
+    </div>
+  );
+}
